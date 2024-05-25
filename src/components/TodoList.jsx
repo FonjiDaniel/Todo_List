3e899d9a2da8104a0a0a663c39630873aec1c0a0
FonjiDaniel <fonjidaniel28@gmail.com>
2024-05-25 16:24:19 +1300

    added add a new task text

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -15,8 +15,11 @@ const TodoList =({ todos, toggleTodo, DeleteTodo }) => {
     <div className='flex justify-center'>
       <p className='p-5 text-lg '>Tasks For Today</p>
       </div>
-      <div className='flex justify-center rounded-2xl '>
+     
+      <div className='flex  justify-center rounded-2xl mt-1'>
+      
     <ol className='p-10 border-2 w-[800px] rounded-2xl flex flex-col gap-3 '>
+    {todos.length === 0? <p className='text-center text-xl font-sans text-blue-200'>No task Added Yet!</p> : null}
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
@@ -28,7 +31,7 @@ const TodoList =({ todos, toggleTodo, DeleteTodo }) => {
     </ol>
     </div>
 
-    <p className='font-bold text-lg text-blue-800 text-center  mt-20 '>copyright @fonjidaniel </p>
+    <p className='font-bold text-lg text-blue-800 text-center  mt-20 '>copyright 😎 </p>
     </div>
     
   );
